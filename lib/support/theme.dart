6022fc8a028c6a8c3d42c6a8e17182c9999e4583
Alastair <alastair.r.mcneill@gmail.com>
2022-08-15 08:36:21 +0100

    Base project setup

---
+++ lib/support/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyThemes {
+  static ThemeData get lightTheme {
+    return ThemeData();
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData();
+  }
+}
+
+class MyColors {}
